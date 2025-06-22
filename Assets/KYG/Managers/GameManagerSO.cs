b810a0c2e9e9b20483bbfb769bff45ee3228ae31
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-06-22 22:43:42 +0900

    [feat] Prototype GameManager
    프로토 타입 구현중
    
    ScriptableObject를 이용한GameManager 구현
    
    게임 일시정지 상태
    
    플레이어 점수
    
    점수 추가
    
    게임 재개 기능

---
+++ Assets/KYG/Managers/GameManagerSO.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameManager", menuName = "Managers/GameManager")]
+public class GameManagerSO : ScriptableObject
+{
+    // 게임 일시 정지 상태
+    [HideInInspector] public bool isGamePaused;
+
+    // 플레이어 점수
+    [HideInInspector] public int score;
+
+    // 점수 추가
+    public void AddScore(int value)
+    {
+        score += value;
+    }
+
+    // 게임 일시 정지
+    public void PauseGame()
+    {
+        isGamePaused = true;
+        Time.timeScale = 0f;
+    }
+
+    // 게임 재개
+    public void ResumeGame()
+    {
+        isGamePaused = false;
+        Time.timeScale = 1f;
+    }
+}
