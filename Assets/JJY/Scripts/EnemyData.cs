d2c95c75534fc8ff503712f9414e91c951a33d85
정재영 <wodud04277@gmail.com>
2025-06-23 22:01:38 +0900

    [Feat] Bullet SingleShot Pattern
    가장 기본적인 단발성 탄막 패턴으로 몬스터의 정면으로 총알이 나감.
    삼점사 탄막 패턴 마저 구현해야함.

--- Assets/JJY/Scripts/EnemyData.cs
+++ Assets/JJY/Scripts/EnemyData.cs
@@ -9,9 +9,9 @@ public class EnemyData : ScriptableObject
     public string EnemyName;
     public int maxHP; // 몬스터 종류에 따라 최대 체력이 다른가?
 
-    // Binary Tree Pattern을 여기서 사용하나?
+    // Binary Tree Pattern을 여기서 어떻게 사용하나?
 
-    // 움직임은 Animator로 만들것.
+    // 움직임은 Animator로 만들것. => Player기준 MainCamera의 위치 정보 필요.
 }
 public class EnemyDropItemData : ScriptableObject
 {
@@ -21,23 +21,4 @@ public class EnemyDropItemData : ScriptableObject
     public float dropRate;
 
     // 플레이어는 주변 아이템을 자력으로 흡수한다. => 아이템은 플레이어에게 어떻게 다가갈 것인가?
-}
-public class BulletPatternData : ScriptableObject
-{
-    [Header("Pattern Basic Info")]
-    public string patternName = "Test SingleShot Pattern"; // 테스트용으로 싱글샷.
-    public GameObject bulletPrefab; // Bullet은 상위 클래스에서 상속 받을것.
-    public float bulletSpeed = 10f;
-    public float fireRate = 0.5f;
-    public int bulletDamage = 1; // 탄환이 주는 피해량 (플레이어의 체력은 몇 단위로 감소될 것인가?)
-
-    [Header("Pattern Type")]
-    public PatternType patternType = PatternType.SingleShot; // 테스트용으로 싱글샷.
-    public enum PatternType
-    {
-        SingleShot, // 단일 발사
-        // 3점사
-        // 부채꼴 (특정 각도사이로 흩뿌리기)
-        // 유도탄 (Player Lock on)
-    }
 }
