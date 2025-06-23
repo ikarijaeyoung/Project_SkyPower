03cf0bc62ec511efc87c46a25016d1c4c81ee021
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-06-23 17:02:49 +0900

    [feat] Prototype ScoreUI
    프로토 타입 구현 중
    
    GameManager를 통해 UIManager 연동 하여 스코어 구현

---
+++ Assets/KYG/Scripts/ScoreUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    public GameManagerSO gameManager;
+    public TMPro.TextMeshPro scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = $"Score: {gameManager.score}";
+    }
+}
