27678e9bb26c1d9867552cfb219a939a60fb8fd2
Lazism2j <lazism2j@gmail.com>
2025-06-24 15:12:40 +0900

    [Chore] 저장 자료 별 함수 구분
    추가할 저장 자료 별 함수를 추가로 구현해야 함

--- Assets/LJ2/SaveManager.cs
+++ Assets/LJ2/SaveManager.cs
@@ -23,20 +23,22 @@ namespace LJ2
             }
         }
 
-
+        // 세이브할 정보 목록 : SaveData 를 상속 받아야 함
         public PlayerSave player;
 
-        public void Save(int index)
+
+        // 정보 별 저장, 로드, 삭제 함수 따로 구현
+        public void PlayerSave(int index)
         {
             DataSaveController.Save(player.saveDataSample, index);
         }
 
-        public void Load(int index)
+        public void PlayerLoad(int index)
         {
             DataSaveController.Load(ref player.saveDataSample, index);
         }
 
-        public void Delete(int index)
+        public void PlayerDelete(int index)
         {
             DataSaveController.Delete(player.saveDataSample, index);
         }
