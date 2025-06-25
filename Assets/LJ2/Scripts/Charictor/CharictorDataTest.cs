c70f0fa16d278c09fc1058286fba1cb72ca58629
Lazism2j <lazism2j@gmail.com>
2025-06-25 20:20:03 +0900

    [Feat] Csv to ScriptableObject
    테스트 환경이지만 구현 성공

---
+++ Assets/LJ2/Scripts/Charictor/CharictorDataTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharictorDataTest", menuName = "Charictor/CharictorDataTest")]
+public class CharictorDataTest : ScriptableObject 
+{
+    public string name;
+    public int level;
+    public int Hp;
+    public int exp;
+    public int attackPower;
+    public int attackSpeed;
+    public int moveSpeed;
+}
--- Assets/LJ2/Scripts/CsvTest.cs
+++ Assets/LJ2/Scripts/CsvTest.cs
@@ -1,16 +1,38 @@
 using IO;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CsvTest : MonoBehaviour
 {
     [SerializeField] private CsvTable table;
+    public CharictorDataTest charictorData;
 
     private void Start()
     {
         CsvReader.Read(table);
+        MakeCharictor();
+    }
 
-        Debug.Log(table.GetData(1, 1));
+    private void MakeCharictor()
+    {
+        for (int i = 1; i < table.Table.GetLength(0); i++)
+        {
+            charictorData = ScriptableObject.CreateInstance<CharictorDataTest>();
+
+            charictorData.name = table.GetData(i, 0);
+            charictorData.level = int.Parse(table.GetData(i, 1));
+            charictorData.Hp = int.Parse(table.GetData(i, 2));
+            charictorData.exp = int.Parse(table.GetData(i, 3));
+            charictorData.attackPower = int.Parse(table.GetData(i, 4));
+            charictorData.attackSpeed = int.Parse(table.GetData(i, 5));
+            charictorData.moveSpeed = int.Parse(table.GetData(i, 6));
+
+            string assetPath = $"Assets/LJ2/Scripts/Charictor/{charictorData.name}.asset";
+            AssetDatabase.CreateAsset(charictorData, assetPath);
+        }
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
     }
 }
