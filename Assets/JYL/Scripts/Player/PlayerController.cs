b4daf094651206fe800c3f95bbe6d4a1fe525eaa
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-06-26 15:59:56 +0900

    [Feat] Add Player move, cut by screen point and UI size
    - 플레이어 이동 구현 완료. 카메라 범위 내에서만 움직일 수 있게 제한한다. 또한, 좌우 양 옆에 있는 UI를 통해서도 이동범위가 제한 될 수 있다. RectTransform을 받아서 그 크기를 구한 후, WorldToViewportPoint 전환 작업을 통해서 비교할 수 있다.

--- Assets/JYL/Scripts/Player/PlayerController.cs
+++ Assets/JYL/Scripts/Player/PlayerController.cs
@@ -1,39 +1,65 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 namespace JYL
 {
     public class PlayerController : MonoBehaviour
     {
         [Header("Set Scriptable Object")]
-        [SerializeField] PlayerModel playerModel;
+        [SerializeField] PlayerModel playerModel; // -> 캐릭터 컨트롤러로 대체 예정
+        // CharacterController[] character -> 캐릭터 3명을 배열로 받음. 게임매니저의 파티조합을 가져옴
 
         [Header("Set References")]
+        [SerializeField] List<ObjectPool> bulletPools;
         [SerializeField] Transform muzzlePoint;
+        [SerializeField] RectTransform leftUI;
+        [SerializeField] RectTransform rightUI;
+        // TODO : UI 시스템 구축 후, 시스템에서 불러오는 식으로 참조 시킨다
+
 
         [Header("Set Value")]
-        [SerializeField] float bulletReturnTimer = 2f;
-        [SerializeField] List<ObjectPool> bulletPools;
+        [Range(0.1f, 5)][SerializeField] float bulletReturnTimer = 2f;
+        [Range(0.1f, 3)][SerializeField] float fireDelay = 2f;
+
+        private PlayerInput playerInput;
+        private Rigidbody rig;
+        private InputAction attackAction;
+        private InputAction parryAction1;
+        private InputAction parryAction2;
+        private InputAction ultAction;
+        private InputAction menuAction;
+
+        // 좌, 우 UI 사이즈
+        private float leftMargin;
+        private float rightMargin;
 
         private int level;
         private int hp;
-
         private int poolIndex = 0;
-        private ObjectPool curBulletPool => bulletPools[poolIndex];
+        private Vector2 inputDir;
+        private bool isAttack;
 
+        private ObjectPool curBulletPool => bulletPools[poolIndex];
+        private void Awake()
+        {
+            Init();
+        }
         private void OnEnable()
         {
             //CreatePlayer();
+            rig = GetComponent<Rigidbody>();
+            SubscribeEvents();
         }
         private void Update()
         {
             PlayerHandler();
-            Debug.Log($"{curBulletPool}");
         }
 
         private void FixedUpdate()
         {
-            PlayerMove();
+
         }
 
         private void LateUpdate()
@@ -43,20 +69,60 @@ namespace JYL
 
         private void OnCollisionEnter(Collision collision)
         {
-            
+            // 적 총알에 맞으면 피격
         }
-        //private void CreatePlayer(CharacterController character)
-        //{
-        // 플레이어 생성
-        //}
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
 
+        //private void CreatePlayer()
+        //{
+        //   플레이어 생성 - 게임매니저의 파티조합을 가져옴. 
+        //   Instantiate(character[0].prefab, transform); -> 캐릭터 컨트롤러에 있는 프리팹을 통해 캐릭터 생성
+        //   
+        //}
+        private void Init()
+        {
+            playerInput = GetComponent<PlayerInput>();
+            attackAction = playerInput.actions["Attack"];
+            // parryAction1 = playerInput.actions["parry1"];
+            // parryAction2 = playerInput.actions["parry2"];
+            // ultAction = playerInput.actions["Ult"];
+            // menuAction = playerInput.actions["menu"];
+
+
+            // TODO: UI 시스템 구축 후, UI Manager에서 참조하는 식으로 변경
+            if(leftUI!= null)
+            {
+                leftMargin = leftUI.rect.width/Camera.main.pixelWidth;
+            }
+            else
+            {
+                leftMargin = 0.1f;
+                Debug.LogWarning("왼쪽 UI 참조 안됐음");
+            }
+            if (rightUI != null)
+            {
+                rightMargin = 1- rightUI.rect.width/Camera.main.pixelWidth;
+            }
+            else
+            {
+                rightMargin = 0.9f;
+                Debug.LogWarning("오른쪽 UI 참조 안됐음");
+            }
+        }
+        private void SubscribeEvents()
+        {
+            attackAction.started += Fire;
+        }
+        private void UnSubscribeEvents()
+        {
+            attackAction.started -= Fire;
+        }
 
         private void PlayerHandler()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Fire();
-            }
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
                 poolIndex = 0;
@@ -65,43 +131,93 @@ namespace JYL
             {
                 poolIndex = 1;
             }
-            if(Input.GetKeyDown(KeyCode.Alpha3))
+            if (Input.GetKeyDown(KeyCode.Alpha3))
             {
                 poolIndex = 2;
             }
+            SetMove();
             //UseUlt
             //Parry
         }
 
-        private void PlayerMove()
+        private void SetMove()
         {
-
+            Vector2 clampInput = ClampMoveInput(inputDir);
+            if (clampInput == Vector2.zero)
+            {
+                rig.velocity = Vector3.zero;
+                return;
+            }
+            Vector3 moveDir = new Vector3(clampInput.x, 0f, clampInput.y);
+            rig.velocity = moveDir * playerModel.playerSpeed;
         }
 
-        private void Fire()
+        private Vector2 ClampMoveInput(Vector2 inputDirection)
+        {
+            if (inputDirection == Vector2.zero)
+            {
+                return Vector2.zero;
+            }
+
+            // 카메라 기준 스크린 좌표로 판단
+            //Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+            // 플레이어가 카메라 뒤에 있다는 뜻
+            //if (screenPos.z <= 0) return Vector2.zero;
+
+            //if (screenPos.x <= 0 && inputDirection.x < 0) inputDirection.x = 0;
+            //if (screenPos.x >= Camera.main.pixelWidth && inputDirection.x > 0) inputDirection.x = 0;
+            //if (screenPos.y <= 0 && inputDirection.y < 0) inputDirection.y = 0;
+            //if (screenPos.y >= Camera.main.pixelHeight && inputDirection.y > 0) inputDirection.y = 0;
+
+            // 뷰포트 기준 좌표로 좀 더 간단화 가능
+            // 뷰포트는 0~1사이의 값으로만 정해져 있다. 매우 정확하게 떨어지진 않겠지만, 어느정도 커버가 된다
+
+            Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+            if(viewportPos.z<=0) return Vector2.zero;
+
+            if (viewportPos.x <= leftMargin && inputDirection.x < 0) inputDirection.x = 0;
+            if (viewportPos.x >= rightMargin && inputDirection.x > 0) inputDirection.x = 0;
+
+            if (viewportPos.y <= 0 && inputDirection.y < 0) inputDirection.y = 0;
+            if (viewportPos.y >= 1 && inputDirection.y > 0) inputDirection.y = 0;
+
+            return inputDirection;
+        }
+
+        public void OnMove(InputValue value)
+        {
+            inputDir = value.Get<Vector2>();
+        }
+
+        private void Fire(InputAction.CallbackContext ctx)
         {
             BulletPrefabController bullet = curBulletPool.ObjectOut() as BulletPrefabController;
             bullet.transform.position = muzzlePoint.position;
             bullet.ReturnToPool(bulletReturnTimer);
-            foreach(BulletInfo info in bullet.bullet)
+            foreach (BulletInfo info in bullet.bullet)
             {
-                if(info.rig == null)
+                if (info.rig == null)
                 {
                     continue;
                 }
                 info.trans.gameObject.SetActive(true);
-                info.trans.position = info.originPos;
+                info.trans.localPosition = info.originPos;
                 info.rig.velocity = Vector3.zero;
                 info.rig.AddForce(playerModel.fireSpeed * info.trans.forward, ForceMode.Impulse);
             }
         }
         //private void UseUlt()
         //{
-             // 궁극기
+        //  궁극기 -  입력을 통해 들어옴
+        //  if(ultGage>=100)
+        //  character[0].Ult();
         //}
-        //private void Parry(CharacterController character)
+
+        //private void Parry(int index)
         //{
-              // 들어온 캐릭터에 따른 패링스킬 사용
+        //  들어온 캐릭터에 따른 패링스킬 사용
+        //  character[index].Parry();
         //}
     }
 }
