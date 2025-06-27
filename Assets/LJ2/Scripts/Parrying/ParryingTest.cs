19e60717853d9d863585ce23eae7781dc7cd867d
Lazism2j <lazism2j@gmail.com>
2025-06-27 21:35:49 +0900

    [Feat] parrying Invincible

--- Assets/LJ2/Scripts/Parrying/ParryingTest.cs
+++ Assets/LJ2/Scripts/Parrying/ParryingTest.cs
@@ -9,6 +9,17 @@ public class ParryingTest : MonoBehaviour
     [SerializeField] float parryingRadius;
     [SerializeField] float destroyRadius;
     [SerializeField] LayerMask destroyLayer;
+    [SerializeField] Collider collider;
+    private Coroutine coroutine;
+
+
+    public YieldInstruction coroutineDelay;
+
+    private void Awake()
+    {
+        coroutineDelay = new WaitForSeconds(invincibleTime);
+        collider = GetComponent<Collider>();
+    }
 
     private void Update()
     {
@@ -20,8 +31,37 @@ public class ParryingTest : MonoBehaviour
 
     public void Parrying()
     {
+        Collider[] canParrying = Physics.OverlapCapsule(transform.position, Vector3.one * parryingRadius, 1, destroyLayer);
+        Debug.Log("패링 진입");
+        if (canParrying.Length > 0)
+        {
+            Debug.Log("조건문 진입");
+
+            foreach (Collider c in canParrying)
+            {
+                c.gameObject.SetActive(false);
+                Debug.Log("반복문 진입");
+
+            }
+            coroutine = StartCoroutine(InvincibleCoroutine());
+
+        }
+        else return;
 
     }
+    
+    private IEnumerator InvincibleCoroutine()
+    {
+        Debug.Log("코루틴 진입");
+
+        collider.enabled = false;
+        yield return coroutineDelay;
+        collider.enabled = true;
+        coroutine = null;
+        yield break;
+
+    }
+
 
 
 }
