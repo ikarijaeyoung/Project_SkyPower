794c1c582e2d681aa53adc209c7e8343a102e29a
정재영 <wodud04277@gmail.com>
2025-07-07 21:38:11 +0900

    [Feat] Twitst Circle Shot, SucflowerShot
    총구를 회전하며 총알을 쏘는 탄막패턴 추가

---
+++ Assets/JJY/Scripts/BulletPatternData/SunflowerShot.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JYL;
+
+[CreateAssetMenu(fileName = "SunflowerShot", menuName = "ScriptableObject/BulletPattern/SunflowerShot")]
+public class SunflowerShot : BulletPatternData
+{
+    [Header("Sunflower Shot Settings")]
+    public int shotCount = 8;
+    public int sunflowerCount = 3;
+    public float twistAnglePerSunflower = 30f;
+    public float fireDelayBetweenShots = 0f;
+    public float fireDelayBetweenSunflower = 0.2f;
+    public float returnToPoolTimer = 5f;
+    public override IEnumerator Shoot(Transform[] firePoints, float bulletSpeed, ObjectPool pool, int attackPower)
+    {
+        Quaternion[] originalRotations = new Quaternion[firePoints.Length];
+        for (int i = 0; i < firePoints.Length; i++)
+        {
+            originalRotations[i] = firePoints[i].rotation;
+        }
+
+        for (int j = 0; j < sunflowerCount; j++)
+        {
+            bool twistThisSunflower = (j % 2 == 1); 
+
+            for (int i = 0; i < shotCount; i++)
+            {
+                BulletPrefabController bulletPrefab = pool.ObjectOut() as BulletPrefabController;
+
+                if (bulletPrefab != null)
+                {
+                    int idx = i % firePoints.Length;
+
+                    firePoints[idx].rotation = originalRotations[idx];
+
+                    if (twistThisSunflower)
+                    {
+                        float twistAngle = (idx % 2 == 0) 
+                            ? twistAnglePerSunflower 
+                            : -twistAnglePerSunflower;
+
+                        firePoints[idx].Rotate(0f, twistAngle, 0f);
+                    }
+
+                    Transform curFirePoint = firePoints[idx];
+
+                    bulletPrefab.objectPool = pool;
+                    bulletPrefab.ReturnToPool(returnToPoolTimer);
+
+                    foreach (BulletInfo info in bulletPrefab.bulletInfo)
+                    {
+                        if (info.rig != null)
+                        {
+                            info.trans.gameObject.SetActive(true);
+                            info.trans.localPosition = info.originPos;
+                            info.trans.position = curFirePoint.position;
+                            info.trans.rotation = Quaternion.LookRotation(curFirePoint.forward);
+                            info.rig.velocity = Vector3.zero;
+                            info.bulletController.attackPower = attackPower;
+                            info.rig.AddForce(curFirePoint.forward * bulletSpeed, ForceMode.Impulse);
+                        }
+                    }
+                }
+                yield return new WaitForSeconds(fireDelayBetweenShots);
+            }
+            yield return new WaitForSeconds(fireDelayBetweenSunflower);
+        }
+    }
+}
