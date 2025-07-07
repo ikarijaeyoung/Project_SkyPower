01f69e98c3a5d7d007df6c92e993c65879c5df38
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-07-08 02:23:14 +0900

    [feat] Dialog
    Sky Power 개발 중
    
    DialogLine
    한 줄의 대사 정보를 담는 데이터 클래스 구현
    
    DialogDB (ScriptableObject)
    여러 개의 대사(DialogLine) 리스트를 가지고 있는 SO 에셋 구현
    
    DialogCSVToSO
    csv 파일을 DialogDB ScriptableObject로 변환하는 에디터 전용 툴 구현
    
    DialogueManager
    실제 게임에서 대사를 순차적으로 관리, 이벤트(콜백)로 UI, 사운드, 연출과 연동 가능
    
    DialogueUI
    DialogueManager에서 받은 대사 정보를 바탕으로 UI에 표시 가능

--- Assets/KYG/Sky Power/Dialog/DialogueUI.cs
+++ Assets/KYG/Sky Power/Dialog/DialogueUI.cs
@@ -1,48 +1,36 @@
-using KYG.SkyPower.Dialogue;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 namespace KYG.SkyPower.Dialogue
 {
-    /// <summary>
-    /// 다이얼로그 UI를 관리하는 컴포넌트입니다.
-    /// </summary>
-    /// <remarks>
-    /// 이 컴포넌트는 DialogueManagerSO와 연결되어 다이얼로그의 상태에 따라 UI를 업데이트합니다.
-    /// </remarks>
-
-public class DialogueUI : MonoBehaviour
-{
-    public TMP_Text speakerText;
-    public TMP_Text contentText;
-    public GameObject rootPanel; // 다이얼로그 UI 전체
-
-    void OnEnable()
+    public class DialogueUI : MonoBehaviour
     {
-        DialogueManagerSO.Instance.OnLineUpdated.AddListener(UpdateUI);
-        DialogueManagerSO.Instance.OnDialogueStarted.AddListener(OpenUI);
-        DialogueManagerSO.Instance.OnDialogueEnded.AddListener(CloseUI);
-    }
+        public TMP_Text speakerText;
+        public TMP_Text dialogText;
+        public Image portrait;
 
-    void OnDisable()
-    {
-        DialogueManagerSO.Instance.OnLineUpdated.RemoveListener(UpdateUI);
-        DialogueManagerSO.Instance.OnDialogueStarted.RemoveListener(OpenUI);
-        DialogueManagerSO.Instance.OnDialogueEnded.RemoveListener(CloseUI);
-    }
+        public DialogueManager manager;
 
-    void UpdateUI(string speaker, string content)
-    {
-        rootPanel.SetActive(true);
-        speakerText.text = speaker;
-        contentText.text = content;
+        void OnEnable()
+        {
+            manager.OnDialogLine.AddListener(UpdateUI);
+        }
+        void OnDisable()
+        {
+            manager.OnDialogLine.RemoveListener(UpdateUI);
+        }
+        void UpdateUI(DialogLine line)
+        {
+            if (line == null)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+            gameObject.SetActive(true);
+            speakerText.text = line.speaker;
+            dialogText.text = line.text;
+            portrait.sprite = line.portrait;
+        }
     }
-    void OpenUI() => rootPanel.SetActive(true);
-    void CloseUI() => rootPanel.SetActive(false);
-
-    public void OnNextButton() => DialogueManagerSO.Instance.NextLine();
-}
 }
