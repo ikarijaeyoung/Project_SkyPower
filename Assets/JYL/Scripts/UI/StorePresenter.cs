54f3c05fbdfdede1fe62a8b7527aa68278327ca0
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-07-08 04:02:03 +0900

    [Feat] Shop Gacha System
     - 가챠 시스템 구현 중

--- Assets/JYL/Scripts/UI/StorePresenter.cs
+++ Assets/JYL/Scripts/UI/StorePresenter.cs
@@ -2,33 +2,44 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using KYG_skyPower;
+using TMPro;
+using LJ2;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 
 namespace JYL
 {
     public class StorePresenter : BaseUI
     {
 
+        private TMP_Text unitText => GetUI<TMP_Text>("StoreUnitText");
+        private CharacterSaveLoader characterLoader;
+        private EquipController equipController;
         void Start()
         {
-            GetEvent("GachaChrBtn1").Click += CharacterGacha;
-            GetEvent("GachaChrBtn5").Click += EquipmentGacha;
-            GetEvent("GachaEquipBtn1").Click += CharacterGacha;
-            GetEvent("GachaEquipBtn5").Click += EquipmentGacha;
-            GetEvent("StoreItemImg").Click += ItemStore;
+
+            GetEvent("GachaChrBtn1").Click += CharGachaClick;
+            GetEvent("GachaChrBtn5").Click += CharGachaClick;
+            GetEvent("GachaEquipBtn1").Click += EquipGachaClick;
+            GetEvent("GachaEquipBtn5").Click += EquipGachaClick;
+            //GetEvent("StoreItemImg").Click += ItemStore;
+            characterLoader = gameObject.GetOrAddComponent<CharacterSaveLoader>();
+            equipController = gameObject.GetOrAddComponent<EquipController>();
+            characterLoader.GetCharPrefab(); // 이큅컨트롤러가 먼저 초기화 된다.
         }
         private void Update()
         {
-            if(Input.GetKeyDown(KeyCode.Escape))
-            {
-            }
-            if(Input.GetKeyDown(KeyCode.Escape)&&!PopUpUI.IsPopUpActive&&!Util.escPressed)
+            if (Input.GetKeyDown(KeyCode.Escape) && !PopUpUI.IsPopUpActive && !Util.escPressed)
             {
                 // 씬 전환
                 SceneManager.LoadSceneAsync("bMainScene_JYL");
+                Util.ConsumeESC();
             }
         }
-        
-        private void CharacterGacha(PointerEventData eventData)
+
+        private void CharGachaClick(PointerEventData eventData)
         {
             // TODO : 가챠 수행과 동시에, 인벤토리(캐릭터 목록)에 추가. 게임매니저에서 세이브함
             // 저장 완료 후, 가챠 연출 진행. 연출 완료후 결과 팝업 창을 띄움
@@ -36,35 +47,70 @@ namespace JYL
             switch (num)
             {
                 case 1:
-                    Manager.Game.CurrentSave.gold -= 200;
-                    UIManager.Instance.ShowPopUp<GachaPopUp>();
+                    if (Manager.Game.CurrentSave.gold >= 100)
+                    {
+                        Manager.Game.CurrentSave.gold -= 100;
+                        
+                        
+                        Manager.Game.SaveGameProgress();
+                        UIManager.Instance.ShowPopUp<GachaPopUp>();
+                    }
                     break;
                 case 5:
-                    Manager.Game.CurrentSave.gold -= 500;
-                    Manager.Game.SaveGameProgress();
-                    UIManager.Instance.ShowPopUp<Gacha5PopUp>();
+                    if(Manager.Game.CurrentSave.gold >= 500)
+                    {
+                        Manager.Game.CurrentSave.gold -= 500;
+                        
+                        
+                        Manager.Game.SaveGameProgress();
+                        UIManager.Instance.ShowPopUp<Gacha5PopUp>();
+                    }
                     break;
             }
         }
-        private void EquipmentGacha(PointerEventData eventData)
+        private void EquipGachaClick(PointerEventData eventData)
         {
             Util.ExtractTrailNumber(eventData.pointerClick.name, out int num);
             switch (num)
             {
                 case 1:
-                    UIManager.Instance.ShowPopUp<GachaPopUp>();
+                    if (Manager.Game.CurrentSave.gold >= 100)
+                    {
+                        Manager.Game.CurrentSave.gold -= 100;
+
+
+                        Manager.Game.SaveGameProgress();
+                        UIManager.Instance.ShowPopUp<GachaPopUp>();
+                    }
                     break;
                 case 5:
-                    UIManager.Instance.ShowPopUp<Gacha5PopUp>();
+                    if (Manager.Game.CurrentSave.gold >= 500)
+                    {
+                        Manager.Game.CurrentSave.gold -= 500;
+
+
+                        Manager.Game.SaveGameProgress();
+                        UIManager.Instance.ShowPopUp<Gacha5PopUp>();
+                    }
                     break;
             }
         }
-
-        // TODO : 아이템 추가 시 작업
-        private void ItemStore(PointerEventData eventData)
+        private List<CharactorController> CharGachaRoll(int num)
         {
+            List<CharactorController> results = new List<CharactorController>(num);
+            return null;
+        }
+        private List<EquipInfo> EquipGachaRoll(int num)
+        {
+            List<EquipInfo> results = new List<EquipInfo>(num);
+            return null;
 
         }
+        //// TODO : 아이템 추가 시 작업
+        //private void ItemStore(PointerEventData eventData)
+        //{
+
+        //}
     }
 }
 
